c1e38314b49cfedc6204c92e3a1f41733d8ed4a9
Ajopratama1 <a.ajopratama01@gmail.com>
2026-03-13 00:35:01 +0700

    buat repository Ktp untuk akses database

---
+++ src/main/java/com/example/ktp/repository/KtpRepository.java
@@ -0,0 +1,9 @@
+package com.example.ktp.repository;
+
+import com.example.ktp.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, String> {
+}
